2285aa4274e54777031f688909b0608aae4ed5a6
GiuliaSossai <giulia.sossai@yahoo.it>
2021-11-05 15:07:57 +0100

    correzione - niente html

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 /**
  * 1. creo array di oggetti, dove ogni oggetto è una card:
  *  - l'oggetto avrà 3 proprietà ovvero immagine nome  e ruolo
- * 2. faccio un ciclo per leggere tutte le info dell'array
- * 3. stampo in html tutto l'array (ovvero le card)
- * 4. aggiugno tramite il form la nuova card
- * - creo nuovo oggetto
+ * 2. faccio un ciclo per leggere tutte le info dell'array: tramite funzione
+ * 3. stampo in html tutto l'array (ovvero le card) - con funzione genero html
+ * 4. aggiungo tramite il form la nuova card
+ * - creo nuovo oggetto al click dell'invio del form
  * - faccio un push all'array per aggiungere la nuova card
- * - faccio un ciclo
  * - stampo in html
  * 
  */
@@ -14,77 +13,121 @@
 //creo array di oggetti/card
 const allCards = [
   {
-    immagine:'img/wayne-barnett-founder-ceo.jpg',
+    immagine:'wayne-barnett-founder-ceo.jpg',
     nome:'Wayne Barnett',
     ruolo:'Founder & CEO'
   },
   {
-    immagine:'img/angela-caroll-chief-editor.jpg',
+    immagine:'angela-caroll-chief-editor.jpg',
     nome:'Angela Caroll',
     ruolo:'Chief Editor'
   },
   {
-    immagine:'img/walter-gordon-office-manager.jpg',
+    immagine:'walter-gordon-office-manager.jpg',
     nome:'Walter Gordon',
     ruolo:'Office Manager'
   },
   {
-    immagine:'img/angela-lopez-social-media-manager.jpg',
+    immagine:'angela-lopez-social-media-manager.jpg',
     nome:'Angela Lopez',
     ruolo:'Social Media Manager'
   },
   {
-    immagine:'img/scott-estrada-developer.jpg',
+    immagine:'scott-estrada-developer.jpg',
     nome:'Scott Estrada',
     ruolo:'Developer'
   },
   {
-    immagine:'img/barbara-ramos-graphic-designer.jpg',
+    immagine:'barbara-ramos-graphic-designer.jpg',
     nome:'Barbara Ramos',
     ruolo:'Graphic Designer'
   }
 ]
 
-let htmlText = '';
+const bottoneAdd = document.getElementById('addMemberButton');
+console.log(bottoneAdd);
+bottoneAdd.addEventListener('click', addMember);
 
-//eseguo ciclo su array per leggerne contenuto
-for(let index in allCards){
-  const card = allCards[index];
-  console.log('card', card);
+takeTeam();
 
-  const cardImg = card.immagine;
-  const cardName = card.nome;
-  const cardRole = card.ruolo;
-  
-  console.log(cardImg);
-  console.log(cardName);
-  console.log(cardRole);
-  console.log('----->');
 
-  //genero html chiamando la funzione
-  htmlText += `
-      <div class="team-card">
-       <div class="card-image">
-        <img src="${cardImg}"/>
-       </div>
-       <div class="card-text">
-         <h3>${cardName}</h3>
-         <p>${cardRole}</p>
-        </div>
-      </div>
-      `;
+function takeTeam (){
+  //reset ambiente dove stampo la prima volta
+  document.querySelector('.team-container').innerHTML = '';
+
+  //eseguo ciclo su array per leggerne contenuto
+  for(let member in allCards){
+    // const card = allCards[index];
+    // console.log('card', card);
+
+    // const cardImg = card.immagine;
+    // const cardName = card.nome;
+    // const cardRole = card.ruolo;
+    
+    // console.log(cardImg);
+    // console.log(cardName);
+    // console.log(cardRole);
+    // console.log('----->');
+
+    generateHtml(member);  
+  }
   
 }
 
 
-//stampo il contenuto in html
-document.querySelector('.team-container').innerHTML = htmlText
+//creo funzione per generare html di ogni membro del team
+function generateHtml(member){
+  //ogni volta aggiungo un membro a quello che già c'è
+  //non voglio sovrascrivere!!
+  //leggo il contenuto di team-container
+  //genero output html
+  //infine lo aggiungo all'html già presente!!!
 
+  const container = document.querySelector('.team-container');
 
+  let prevContent = container.innerHTML;
 
+  //destrutturo oggetto
+  const {immagine, nome, ruolo} = member;
 
-//creo funzione per generare html
-function generateHtml(){
+  //definisco struttura html
+  prevContent +=
+  `
+  <div class="team-card">
+    <div class="card-image">
+      <img src="img/${immagine}"/>
+    </div>
+    <div class="card-text">
+      <h3>${nome}</h3>
+      <p>${ruolo}</p>
+    </div>
+  </div>
+  `;
+
+  //console.log(prevContent);
+
+  container.innerHTML = prevContent;
   
+}
+
+//funzione chiamata dal click sul bottone del form
+function addMember(){
+  //leggo i valori dell'oggetto
+  //creo l'oggetto
+  //invio oggetto creato alla funzione generateHTML
+
+  const immagineNew = document.getElementById('image').ariaValueMax;
+  const nomeNew = document.getElementById('name').ariaValueMax;
+  const ruoloNew = document.getElementById('role').ariaValueMax;
+
+  const newMember = {
+    image: immagineNew,
+    name: nomeNew,
+    role: ruoloNew
+  }
+
+  generateHtml(newMember)
+  
+
 }
   
